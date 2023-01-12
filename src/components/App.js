665f882a6393d00dd35dd5cfa6eec8141ae0b7c8
AntaresScorpius <rohitc171@gamil.com>
2023-01-12 20:56:08 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol>
+        <li key="relativeListItem1">Realtive one</li>
+        <li key="relativeListItem2">Realtive two</li>
+        <li key="relativeListItem3">Realtive three</li>
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
